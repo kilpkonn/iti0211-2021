f6534f1f8bb8d5bb8efc812ea27cc1b8667c5482
Tavo Annus <tavo.annus@gmail.com>
2021-09-04 21:32:40 +0300

    Test

--- PR01/prax00.pl
+++ PR01/prax00.pl
@@ -1,1 +1,22 @@
-lendab(kajakas).
+kass(miisu).
+kass(ants).
+kass(peedu).
+koer(muki).
+koer(pontu).
+koer(reks).
+rott(ruudi).
+
+lemmik(tiina,koer,pontu).
+lemmik(tiina,kass,miisu).
+lemmik(tiina,papagoi,kiki).
+lemmik(tiina,rott,ruudi).
+lemmik(piret,koer,muki).
+lemmik(piret,kass,peedu).
+lemmik(peeter,kass,ants).
+lemmik(peeter,koer,reks).
+
+maja(ants,punane).
+mängib(jüri, korvpall).
+omab(X, kana) :- maja(X, sinine).
+omab(X, kanaarilind) :- maja(X, roheline).
+trenn(X, jalka) :- omab(X, koer).
