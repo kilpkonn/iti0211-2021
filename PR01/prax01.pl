a49cd2c35f3bb8cdb28d1d605c6696584ffde809
tavo <taannu@ttu.ee>
2021-09-07 16:36:56 +0300

    ...

--- PR01/prax01.pl
+++ PR01/prax01.pl
@@ -3,6 +3,9 @@ male(evo).
 male(tarmo).
 male(ahto).
 male(mati).
+male(teet).
+male(kurmo).
+male(ruudo).
 female(liis).
 female(terje).
 female(malle).
@@ -12,10 +15,24 @@ married(terje, tarmo).
 married(malle, ahto).
 married(tiiu, mati).
 
-mother(terje, tavo).
-mother(terje, evo).
-mother(terje, liis).
+mother(tavo, terje).
+mother(evo, terje).
+mother(liis, terje).
+mother(terje, tiiu).
+mother(teet, tiiu).
+mother(kurmo, malle).
+mother(ruudo, malle).
+mother(tarmo, malle).
 
-mother(tiiu, terje).
-mother(malle, tarmo).
+father(Child, Father) :- mother(Child, Mother), married(Mother, Father).
+brother(Child, Brother) :- mother(Child, Mother), mother(Brother, Mother), male(Brother).
+sister(Child, Sister) :- mother(Child, Mother), mother(Sister, Mother), female(Sister).
+aunt(Child, Aunt) :- mother(Child, Mother), sister(Mother, Aunt).
+aunt(Child, Aunt) :- father(Child, Father), sister(Father, Aunt).
+uncle(Child, Uncle) :- father(Child, Father), brother(Father, Uncle).
+uncle(Child, Uncle) :- mother(Child, Mother), brother(Mother, Uncle).
+grandfather(Child, Grandfather) :- father(Child, Father), father(Father, Grandfather).
+grandfather(Child, Grandfather) :- mother(Child, Father), father(Father, Grandfather).
+grandmother(Child, Grandmother) :- father(Child, Mother), mother(Mother, Grandmother).
+grandmother(Child, Grandmother) :- mother(Child, Mother), mother(Mother, Grandmother).
 
