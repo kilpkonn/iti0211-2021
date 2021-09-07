7cfc0566f6a626b2058ec60dd3442ca5e7dd980a
tavo <taannu@ttu.ee>
2021-09-07 16:13:34 +0300

    ...

--- PR01/prax01.pl
+++ PR01/prax01.pl
@@ -1,1 +1,21 @@
 male(tavo).
+male(evo).
+male(tarmo).
+male(ahto).
+male(mati).
+female(liis).
+female(terje).
+female(malle).
+female(tiiu).
+
+married(terje, tarmo).
+married(malle, ahto).
+married(tiiu, mati).
+
+mother(terje, tavo).
+mother(terje, evo).
+mother(terje, liis).
+
+mother(tiiu, terje).
+mother(malle, tarmo).
+
