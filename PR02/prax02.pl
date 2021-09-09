c895cc273c078f824cf0f1b6f3f576106c03983b
Tavo Annus <tavo.annus@gmail.com>
2021-09-09 21:45:35 +0300

    Add stuff

--- PR02/prax02.pl
+++ PR02/prax02.pl
@@ -8,17 +8,25 @@ male(teet).
 male(kurmo).
 male(ruudo).
 male(matiIsa).
+male(tiiuIsa).
 male(ahtoIsa).
+male(malleIsa).
 female(liis).
 female(terje).
 female(malle).
 female(tiiu).
 female(tiiuEma).
+female(matiEma).
 female(malleEma).
+female(ahtoEma).
 
 married(terje, tarmo).
 married(malle, ahto).
 married(tiiu, mati).
+married(tiiuEma, tiiuIsa).
+married(ahtoEma, ahtoIsa).
+married(matiEma, matiIsa).
+married(malleEma, malleIsa).
 
 mother(tavo, terje).
 mother(evo, terje).
@@ -30,16 +38,21 @@ mother(ruudo, malle).
 mother(tarmo, malle).
 mother(tiiu, tiiuEma).
 mother(malle, malleEma).
+mother(mati, matiEma).
+mother(ahto, ahtoEma).
 
 father(Child, Father) :- mother(Child, Mother), married(Mother, Father).
+
 brother(Child, Brother) :- mother(Child, Mother), mother(Brother, Mother), male(Brother), Brother \= Child.
-%# brother(Child, Brother) :- father(Child, Father), father(Brother, Father), male(Brother).
+brother(Child, Brother) :- father(Child, Father), father(Brother, Father), male(Brother), Brother \= Child.
 sister(Child, Sister) :- mother(Child, Mother), mother(Sister, Mother), female(Sister), Sister \= Child.
-%# sister(Child, Sister) :- father(Child, Father), father(Sister, Father), female(Sister).
+sister(Child, Sister) :- father(Child, Father), father(Sister, Father), female(Sister), Sister \= Child.
+
 aunt(Child, Aunt) :- mother(Child, Mother), sister(Mother, Aunt).
 aunt(Child, Aunt) :- father(Child, Father), sister(Father, Aunt).
 uncle(Child, Uncle) :- father(Child, Father), brother(Father, Uncle).
 uncle(Child, Uncle) :- mother(Child, Mother), brother(Mother, Uncle).
+
 grandfather(Child, Grandfather) :- father(Child, Father), father(Father, Grandfather).
 grandfather(Child, Grandfather) :- mother(Child, Father), father(Father, Grandfather).
 grandmother(Child, Grandmother) :- father(Child, Mother), mother(Mother, Grandmother).
@@ -52,8 +65,8 @@ anchestor(Child, Parent) :- mother(Child, X), anchestor(X, Parent).
 male_ancestor(Child, Parent) :- male(Parent), anchestor(Child, Parent).
 female_ancestor(Child, Parent) :- female(Parent), anchestor(Child, Parent).
 
-anchestor1(Child, Parent, N) :- father(Child, Parent), N = 1.
-anchestor1(Child, Parent, N) :- mother(Child, Parent), N = 1.
+anchestor1(Child, Parent, N) :- father(Child, Parent), N == 1.
+anchestor1(Child, Parent, N) :- mother(Child, Parent), N == 1.
 anchestor1(Child, Parent, N) :- father(Child, X), anchestor1(X, Parent, M), N is M + 1.
 anchestor1(Child, Parent, N) :- mother(Child, X), anchestor1(X, Parent, M), N is M + 1.
 
