634b42b3b18d2b119242224a13bf8d07d4933147
tavo <taannu@ttu.ee>
2021-09-21 17:02:48 +0300

    .

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -19,7 +19,7 @@ reisi(X, Y, Path) :- (laevaga(X, Y, _); bussiga(X, Y, _); rongiga(X, Y, _); lenn
 reisi(X, Y, Path) :- 
   (laevaga(X, Z, _); bussiga(X, Z, _); rongiga(X, Z, _); lennukiga(X, Z, _)),
   Path = mine(X, Z, SubPath),
-  reisi(Z, Y, SubPath).
+  reisi(Z, Y, SubPath), !.
 
 reisi_transpordiga(X, Y, Path) :- laevaga(X, Y, _), Path = mine(X, Y, laevaga), !.
 reisi_transpordiga(X, Y, Path) :- bussiga(X, Y, _), Path = mine(X, Y, bussiga), !.
@@ -43,20 +43,20 @@ reisi(X, Y, Path, Cost) :- laevaga(X, Y, Cost), Path = mine(X, Y, laevaga), !.
 reisi(X, Y, Path, Cost) :- bussiga(X, Y, Cost), Path = mine(X, Y, bussiga), !.
 reisi(X, Y, Path, Cost) :- rongiga(X, Y, Cost), Path = mine(X, Y, rongiga), !.
 reisi(X, Y, Path, Cost) :- lennukiga(X, Y, Cost), Path = mine(X, Y, lennukiga), !.
-reisi(X, Y, Path, Cost) :- laevaga(X, Z, C1),
+reisi(X, Y, Path, Cost) :- laevaga(X, Z, CostA),
   Path = mine(X, Z, laevaga, SubPath),
-  Cost is C1 + C2,
-  reisi(Z, Y, SubPath, C2).
-reisi(X, Y, Path, Cost) :- bussiga(X, Z, C1),
+  Cost is CostA + CostB,
+  reisi(Z, Y, SubPath, CostB).
+reisi(X, Y, Path, Cost) :- bussiga(X, Z, CostA),
   Path = mine(X, Z, bussiga, SubPath),
-  Cost is C1 + C2,
-  reisi(Z, Y, SubPath, C2).
-reisi(X, Y, Path, Cost) :- rongiga(X, Z, C1),
+  Cost is CostA + CostB,
+  reisi(Z, Y, SubPath, CostB).
+reisi(X, Y, Path, Cost) :- rongiga(X, Z, CostA),
   Path = mine(X, Z, rongiga, SubPath),
-  Cost is C1 + C2,
-  reisi(Z, Y, SubPath, C2).
-reisi(X, Y, Path, Cost) :- lennukiga(X, Z, C1),
+  Cost is CostA + CostB,
+  reisi(Z, Y, SubPath, CostB).
+reisi(X, Y, Path, Cost) :- lennukiga(X, Z, CostA),
   Path = mine(X, Z, lennukiga, SubPath),
-  Cost is C1 + C2,
-  reisi(Z, Y, SubPath, C2).
+  Cost is CostA + CostB,
+  reisi(Z, Y, SubPath, CostB).
 
