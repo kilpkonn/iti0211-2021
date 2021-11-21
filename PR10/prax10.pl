63394e01f6e2f8d156f54bf058322623322648cd
Tavo Annus <tavo.annus@gmail.com>
2021-11-21 15:40:05 +0200

    .

--- PR10/prax10.pl
+++ PR10/prax10.pl
@@ -53,6 +53,7 @@ kas_saab_votta(X,Y,Suund,X1,Y1,X2,Y2):-  % Votmine tagasi vasakule
     Y2 is Y1 - 1,
     ruut(X2,Y2, 0).
 
+
 tee_kaik(X,Y,X1,Y1) :- retract(ruut(X, Y, C)), retract(ruut(X1, Y1, 0)), assertz(ruut(X, Y, 0)), assertz(ruut(X1, Y1, C)).
 % From, Dir, Over, To
 vota(X,Y,_,X1,Y1,X2,Y2) :- retract(ruut(X1, Y1, _)), assertz(ruut(X1, Y1, 0)), tee_kaik(X, Y, X2, Y2).
